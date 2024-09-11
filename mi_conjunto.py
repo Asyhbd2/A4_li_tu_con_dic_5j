928370fbb89a46c87273c33589080fc51d5e35cd
Angel Tadeo De Leon Ceniceros <a.22308051281165@cbtis128.edu.mx>
2024-09-11 07:37:47 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo de conjuntos Set")
+tipodeportes={"Futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+print("Lista de elementos del conjunto tipodeportes loop for")
+for DeLeon in tipodeportes:
+    print(DeLeon)
